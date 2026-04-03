65875a7392a6a8e555d11467fbe2e47eaaff7736
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 19:27:12 -0500

    style: js-beautify formatting (zfs.mjs)

--- zfs.mjs
+++ zfs.mjs
@@ -8,18 +8,18 @@ import {
 
 export const zreadFileSync = function() {
   try {
-      return readFileSync(...arguments);
+    return readFileSync(...arguments);
   } catch (e) {
-      if (arguments[1]?.encoding || typeof arguments[1] == 'string') {
-          return e.message;
-      }
-      return Buffer.from(e.message);
+    if (arguments[1]?.encoding || typeof arguments[1] == 'string') {
+      return e.message;
+    }
+    return Buffer.from(e.message);
   }
 }
 export const zwriteFileSync = function() {
   try {
-      return writeFileSync(...arguments);
+    return writeFileSync(...arguments);
   } catch (e) {
-      console.log(e);
+    console.log(e);
   }
 };
